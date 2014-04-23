0c0af2f5d595655004c71688cf70824b71dafc77
Kim Slawson <kimslawson@gmail.com>
2014-04-23 16:51:37 -0400

    Add dummy generate function. Add options. Add states to button.

--- js/heartbleed-passphrase.js
+++ js/heartbleed-passphrase.js
@@ -1,8 +1,12 @@
 $(document).ready( function() {
-
+    // make text responsive
     $("html").responsiveType({minSize: 100,
                               maxSize: 150,
                               minWidth: 320,
                               maxWidth: 1280});
 
+    // generate a passphrase with options
+    function generatePassphrase(theForm) {
+        return false;
+    }
 });
